5cf73cf3f9124d7cfcb812128d784388d1d5e77c
Seth Tunstall <seth@tunstall.in>
2024-10-22 12:49:28 +0100

    change shell refs `vault` -> `bao`

--- config/samples/setup.sh
+++ config/samples/setup.sh
@@ -4,24 +4,24 @@
 
 set -e
 
-cat <<EOF | kubectl -n vault exec -i vault-0 -- sh -e
-vault secrets disable kvv2/
-vault secrets enable -path=kvv2 kv-v2
-vault kv put kvv2/secret username="db-readonly-username" password="db-secret-password"
+cat <<EOF | kubectl -n bao exec -i bao-0 -- sh -e
+bao secrets disable kvv2/
+bao secrets enable -path=kvv2 kv-v2
+bao kv put kvv2/secret username="db-readonly-username" password="db-secret-password"
 
-vault secrets disable kvv1/
-vault secrets enable -path=kvv1 -version=1 kv
-vault kv put kvv1/secret username="v1-user" password="v1-password"
+bao secrets disable kvv1/
+bao secrets enable -path=kvv1 -version=1 kv
+bao kv put kvv1/secret username="v1-user" password="v1-password"
 
-vault secrets disable pki
-vault secrets enable pki
-vault write pki/root/generate/internal \
+bao secrets disable pki
+bao secrets enable pki
+bao write pki/root/generate/internal \
     common_name=example.com \
     ttl=768h
-vault write pki/config/urls \
+bao write pki/config/urls \
     issuing_certificates="http://127.0.0.1:8200/v1/pki/ca" \
     crl_distribution_points="http://127.0.0.1:8200/v1/pki/crl"
-vault write pki/roles/default \
+bao write pki/roles/default \
     allowed_domains=example.com \
     allowed_domains=localhost \
     allow_subdomains=true \
@@ -38,14 +38,14 @@ path "pki/*" {
   capabilities = ["read", "create", "update"]
 }
 EOT
-vault policy write demo /tmp/policy.hcl
+bao policy write demo /tmp/policy.hcl
 
 # setup the necessary auth backend
-vault auth disable kubernetes
-vault auth enable kubernetes
-vault write auth/kubernetes/config \
+bao auth disable kubernetes
+bao auth enable kubernetes
+bao write auth/kubernetes/config \
     kubernetes_host=https://kubernetes.default.svc
-vault write auth/kubernetes/role/demo \
+bao write auth/kubernetes/role/demo \
     bound_service_account_names=default \
     bound_service_account_namespaces=tenant-1,tenant-2 \
     policies=demo \
